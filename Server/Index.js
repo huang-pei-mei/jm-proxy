c01ddbe43d58f0fcbe128272925d99ed4b322295
Jacob <ja.metevia@gmail.com>
2021-06-03 10:52:26 -0700

    reverted port number

--- Server/Index.js
+++ Server/Index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const port = 80;
+const port = 4000;
 const path = require('path');
 const axios = require('axios');
 const request = require('request');
