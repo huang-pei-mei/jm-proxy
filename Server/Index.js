726d7baf53481f0dde89e16cab19caead3289ca6
Jacob <ja.metevia@gmail.com>
2021-06-03 14:55:58 -0700

    updated cors

--- Server/Index.js
+++ Server/Index.js
@@ -7,7 +7,7 @@ const request = require('request');
 
 
 app.get('/books/:id/reviews', (req, res) => {
-  res.set({'Access-Control-Allow-Origin': 'http://ec2-54-153-95-228.us-west-1.compute.amazonaws.com:80'})
+  res.set({'Access-Control-Allow-Origin': '*'})
   axios.get(`http://ec2-54-183-2-218.us-west-1.compute.amazonaws.com:4001/books/${req.params.id}/reviews`, {headers: req.headers})
   .then((response) => {
     res.status(202).json(response.data);
@@ -20,7 +20,7 @@ app.get('/books/:id/reviews', (req, res) => {
 
 
 app.get('/books/:id/api/price/', (req, res) => {
-  res.set({'Access-Control-Allow-Origin': 'http://ec2-54-153-95-228.us-west-1.compute.amazonaws.com:80'})
+  res.set({'Access-Control-Allow-Origin': '*'})
   axios.get(`http://ec2-34-221-235-141.us-west-2.compute.amazonaws.com:3000/api/price/${req.params.id}`, {headers: req.headers})
   .then((response) => {
     res.status(202).json(response.data);
@@ -31,7 +31,7 @@ app.get('/books/:id/api/price/', (req, res) => {
 });
 
 app.get('/books/:id/api/book', (req, res) => {
-  res.set({'Access-Control-Allow-Origin' :'http://ec2-54-153-95-228.us-west-1.compute.amazonaws.com:80'});
+  res.set({'Access-Control-Allow-Origin' :'*'});
   axios.get(`http://13.57.14.144:2002/api/book/${req.params.id}`, {headers: req.headers})
   .then((response) => {
     res.status(202).json(response.data);
@@ -43,7 +43,7 @@ app.get('/books/:id/api/book', (req, res) => {
 
 
 app.get('/books/:id/api/summary', (req, res) => {
-  res.set({'Access-Control-Allow-Origin': 'http://ec2-54-153-95-228.us-west-1.compute.amazonaws.com:80'})
+  res.set({'Access-Control-Allow-Origin': '*'})
   axios.get(`http://ec2-18-188-135-5.us-east-2.compute.amazonaws.com:1220/api/summary/${req.params.id}`, {headers: req.headers})
   .then((response) => {
     res.status(202).json(response.data);
@@ -54,7 +54,7 @@ app.get('/books/:id/api/summary', (req, res) => {
 })
 
 app.get('/books/:id/api/aggReview', (req, res) => {
-  res.set({'Access-Control-Allow-Origin' : 'https://s3-us-west-1.amazonaws.com'})
+  res.set({'Access-Control-Allow-Origin' : '*'})
   axios.get(`http://ec2-18-220-21-137.us-east-2.compute.amazonaws.com:2880/api/aggReview/${req.params.id}`, {headers: req.headers})
   .then((response) => {
     res.status(202).json(response.data);
